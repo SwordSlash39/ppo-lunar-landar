5d977729f9da073d5d79159fe8fea3446dbf1b4c
Ernest Tan Jia Teng <57171675+SwordSlash39@users.noreply.github.com>
2025-02-12 16:29:46 +0800

    Update ppo_n_states.py
    Included entropy loss

--- ppo_n_states.py
+++ ppo_n_states.py
@@ -2,6 +2,10 @@ import gymnasium as gym
 import torch
 import torch.nn as nn
 import numpy as np
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+cpu_device = torch.device("cpu")
+# Set all tensors to device
+torch.set_default_tensor_type(torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor)
 
 # Define the policy network
 class Policy(nn.Module):
@@ -51,6 +55,7 @@ if __name__ == '__main__':
     LEARNING_RATE = 1e-3
     GAMMA = 0.99
     EPSILON = 0.2
+    entropy_weight = 0.001
     policy = Policy()
     value = Value()
     policy_optim = torch.optim.Adam(policy.parameters(), lr=LEARNING_RATE)
@@ -58,7 +63,7 @@ if __name__ == '__main__':
 
     # Reset the environment to generate the first observation
     states2, next_states2, old_policy2, actions2, rewards2, done2 = [], [], [], [], [], []
-    observation, info = env.reset(seed=42)
+    observation, info = env.reset()
     terminated = False
     BATCH = 512
     games, curr_game_reward, latest_reward = 0, 0, 0
@@ -69,9 +74,12 @@ if __name__ == '__main__':
             i += 1
             states, next_states, old_policy, actions, rewards, done = [], [], [], [], [], []
             with torch.no_grad():
+                # Use cpu for running batch=1 but gpu for more
+                policy = policy.to(cpu_device)
+                value = value.to(cpu_device)
                 while len(rewards) <= BATCH:
                     # run the policy network on the observation
-                    log_action = policy(torch.tensor(observation, dtype=torch.float32).unsqueeze(0)).squeeze(0)
+                    log_action = policy(torch.tensor(observation, dtype=torch.float32, device=cpu_device).unsqueeze(0)).squeeze(0)
                     action = torch.exp(log_action)
                     
                     # Sample randomly to get action
@@ -114,13 +122,17 @@ if __name__ == '__main__':
                 states2, next_states2, old_policy2, actions2, rewards2, done2 = states, next_states, old_policy, actions, rewards, done
                 continue
             
+            # Convert policy, value back to gpu
+            policy = policy.to(device)
+            value = value.to(device)
+            
             # Convert to tensors
             states_tensor = torch.tensor(np.array(states2), dtype=torch.float32)
             next_states_tensor = torch.tensor(np.array(next_states2), dtype=torch.float32)
             actions_tensor = torch.tensor(actions2, dtype=torch.int64).unsqueeze(1)
             rewards_tensor = torch.tensor(rewards2, dtype=torch.float32).unsqueeze(1)
             done_tensor = torch.tensor(done2, dtype=torch.float32).unsqueeze(1)
-            old_policy_tensor = torch.stack(old_policy2, dim=0)
+            old_policy_tensor = torch.stack(old_policy2, dim=0).to(device)
 
             # rewards_tensor = (rewards_tensor - rewards_tensor.mean()) / (rewards_tensor.std() + 1e-5)
             
@@ -132,6 +144,7 @@ if __name__ == '__main__':
             advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-5)
             r_value = torch.exp(torch.gather(policy(states_tensor), 1, actions_tensor) - torch.gather(old_policy_tensor, 1, actions_tensor).detach())
             policy_loss = -(torch.min(r_value * advantage, advantage * torch.clamp(r_value, 1 - EPSILON, 1 + EPSILON))).mean()
+            entropy = -entropy_weight * (torch.exp(policy(states_tensor)) * policy(states_tensor)).sum(dim=1).mean()
             
             # Update the policy and value networks
             value_optim.zero_grad(set_to_none=True)
@@ -139,6 +152,7 @@ if __name__ == '__main__':
             
             value_loss.backward()
             policy_loss.backward()
+            entropy.backward()
             
             value_optim.step()
             policy_optim.step()
@@ -149,8 +163,7 @@ if __name__ == '__main__':
             # Log
             writer.add_scalar("Value Loss", value_loss.item(), i)
             writer.add_scalar("Policy Loss", policy_loss.item(), i)
-            if i % log == 0:
-                print(f"{i}: Value Loss: {value_loss.item()} | Policy Loss: {policy_loss.item()} | Reward: {latest_reward}")
+            writer.add_scalar("Entropy", entropy.item(), i)
 
     except KeyboardInterrupt:
         env.close()
